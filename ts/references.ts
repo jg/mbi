f896703469e43a03f93cc58e6cc476280764ed67
Juliusz Gonera <juliusz.gonera@gmail.com>
2015-01-13 06:30:07 +0100

    Switch to grunt-ts compilation

--- ts/references.ts
+++ ts/references.ts
@@ -2,3 +2,13 @@
 /// <reference path="./types/underscore.d.ts" />
 /// <reference path="./types/underscore.string.d.ts" />
 /// <reference path="./types/qunit.d.ts" />
+
+//grunt-start
+/// <reference path="main.ts" />
+/// <reference path="test.ts" />
+/// <reference path="types/jquery.d.ts" />
+/// <reference path="types/qunit.d.ts" />
+/// <reference path="types/require.d.ts" />
+/// <reference path="types/underscore.d.ts" />
+/// <reference path="types/underscore.string.d.ts" />
+//grunt-end
