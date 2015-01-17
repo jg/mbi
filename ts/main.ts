76a0230d92a01962190a440b7ab07b3f46eb0e4c
Juliusz Gonera <juliusz.gonera@gmail.com>
2015-01-17 05:39:26 +0100

    Use correct pam250 scoring matrix

--- ts/main.ts
+++ ts/main.ts
@@ -44,49 +44,52 @@ function queryWords(query: string, k: number): Array<QueryWord> {
     });
 }
 
-// http://www.icp.ucl.ac.be/~opperd/private/pam250.html
+// http://prowl.rockefeller.edu/aainfo/pam250.htm
 // Hash lookup table with pam250 values. pam250['A']['R'] = 3
 function pam250(): ScoreMatrix {
-    var lines =
-        ['13 6 9 9 5 8 9 12 6 8 6 7 7 4 11 11 11 2 4 9',
-         '3 17 4 3 2 5 3 2 6 3 2 9 4 1 4 4 3 7 2 2',
-         '4 4 6 7 2 5 6 4 6 3 2 5 3 2 4 5 4 2 3 3',
-         '5 4 8 11 1 7 10 5 6 3 2 5 3 1 4 5 5 1 2 3',
-         '2 1 1 1 52 1 1 2 2 2 1 1 1 1 2 3 2 1 4 2',
-         '3 5 5 6 1 10 7 3 7 2 3 5 3 1 4 3 3 1 2 3',
-         '5 4 7 11 1 9 12 5 6 3 2 5 3 1 4 5 5 1 2 3',
-         '12 5 10 10 4 7 9 27 5 5 4 6 5 3 8 11 9 2 3 7',
-         '2 5 5 4 2 7 4 2 15 2 2 3 2 2 3 3 2 2 3 2',
-         '3 2 2 2 2 2 2 2 2 10 6 2 6 5 2 3 4 1 3 9',
-         '6 4 4 3 2 6 4 3 5 15 34 4 20 13 5 4 6 6 7 13',
-         '6 18 10 8 2 10 8 5 8 5 4 24 9 2 6 8 8 4 3 5',
-         '1 1 1 1 0 1 1 1 1 2 3 2 6 2 1 1 1 1 1 2',
-         '2 1 2 1 1 1 1 1 3 5 6 1 4 32 1 2 2 4 20 3',
-         '7 5 5 4 3 5 4 5 5 3 3 4 3 2 20 6 5 1 2 4',
-         '9 6 8 7 7 6 7 9 6 5 4 7 5 3 9 10 9 4 4 6',
-         '8 5 6 6 4 5 5 6 4 6 4 6 5 3 6 8 11 2 3 6',
-         '0 2 0 0 0 0 0 0 1 0 1 0 0 1 0 1 0 55 1 0',
-         '1 1 2 1 3 1 1 1 3 2 2 1 2 15 1 2 2 3 31 2',
-         '7 4 4 4 4 4 4 4 5 4 15 10 4 10 5 5 5 72 4 17']
+    var lines = [
+        '2',
+        '-2	6',
+        '0	0	2',
+        '0	-1	2	4',
+        '-2	-4	-4	-5	4',
+        '0	1	1	2	-5	4',
+        '0	-1	1	3	-5	2	4',
+        '1	-3	0	1	-3	-1	0	5',
+        '-1	2	2	1	-3	3	1	-2	6',
+        '-1	-2	-2	-2	-2	-2	-2	-3	-2	5',
+        '-2	-3	-3	-4	-6	-2	-3	-4	-2	2	6',
+        '-1	3	1	0	-5	1	0	-2	0	-2	-3	5',
+        '-1	0	-2	-3	-5	-1	-2	-3	-2	2	4	0	6',
+        '-4	-4	-4	-6	-4	-5	-5	-5	-2	1	2	-5	0	9',
+        '1	0	-1	-1	-3	0	-1	-1	0	-2	-3	-1	-2	-5	6',
+        '1	0	1	0	0	-1	0	1	-1	-1	-3	0	-2	-3	1	3',
+        '1	-1	0	0	-2	-1	0	0	-1	0	-2	0	-1	-2	0	1	3',
+        '-6	2	-4	-7	-8	-5	-7	-7	-3	-5	-2	-3	-4	0	-6	-2	-5	17',
+        '-3	-4	-2	-4	0	-4	-4	-5	0	-1	-1	-4	-2	7	-5	-3	-3	0	10',
+        '0	-2	-2	-2	-2	-2	-2	-1	-2	4	2	-2	2	-1	-1	-1	0	-6	-2	4',
+    ]
     var headerString = 
         "A    R    N    D    C    Q    E    G    H    I    L    K    M    F    P    S    T    W    Y    V"
     var headers = _.str.clean(headerString).split(" ")
-
-    var dataArray = _.map(lines, (line) => line.split(" "))
+    
+    var dataArray = _.map(lines, (line) => _.str.clean(line).split(" "))
 
     var pam250: ScoreMatrix = {}
     _.each(headers, (header) => pam250[header] = {})
 
-        _.each(headers, (headerRow) => {
-            _.each(headers, (headerCol) => {
-                var rowIndex = _.indexOf(headers, headerRow)
-                var colIndex = _.indexOf(headers, headerCol)
-                pam250[headerRow][headerCol] = parseInt(dataArray[rowIndex][colIndex])
-            })
+    _.each(dataArray, (row, rowIndex) => {
+        _.each(row, (value, colIndex) => {
+            var rowHeader = headers[rowIndex]
+            var colHeader = headers[colIndex]
+            pam250[rowHeader][colHeader] = parseInt(value)
+            pam250[colHeader][rowHeader] = parseInt(value)
         })
+    })
 
     return pam250;
 }
+
 
 function alphabet(): Array<string> {
     var headerString = "A    R    N    D    C    Q    E    G    H    I    L    K    M    F    P    S    T    W    Y    V"
--- ts/test.ts
+++ ts/test.ts
@@ -1,9 +1,10 @@
 /// <reference path="./references.ts" />
 
 QUnit.test("pam250", (assert) => {
-    assert.equal(pam250()['A']['A'], 13)
-    assert.equal(pam250()['A']['R'], 6)
-    assert.equal(pam250()['R']['A'], 3)
+    assert.equal(pam250()['A']['A'], 2)
+    assert.equal(pam250()['A']['R'], -2)
+    assert.equal(pam250()['R']['A'], -2)
+    assert.equal(pam250()['E']['E'], 4)
 })
 
 
@@ -12,11 +13,23 @@ QUnit.test("alphabet", (assert) => {
 })
 
 QUnit.test("wordPairScore", (assert) => {
-    assert.equal(wordPairScore(pam250(), "PQG", "QGE"), 17)
-    assert.equal(wordPairScore(pam250(), "PQG", "GEF"), 15)
+    assert.equal(wordPairScore(pam250(), "PQG", "QGE"), -1)
+    assert.equal(wordPairScore(pam250(), "PQG", "GEF"), -4)
 })
 
 QUnit.test("highScoringNeighbors / pam250", (assert) => {
-    var expected = ["PRG", "PNG", "PDG", "PQG", "PEG", "PHG", "PKG", "PPG"]
-    assert.deepEqual(highScoringNeighbors(pam250(), "PQG", 50), expected)
+    var expected = 
+        [  "PAG",
+           "PRG",
+           "PNG",
+           "PDG",
+           "PQA",
+           "PQD",
+           "PQG",
+           "PQS",
+           "PEG",
+           "PHG",
+           "PKG",
+           "PPG"]
+    assert.deepEqual(highScoringNeighbors(pam250(), "PQG", 10), expected)
 })
